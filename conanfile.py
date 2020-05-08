63cf6c8238836d7216ad9e0b1de4c3329b799c0e
Keef Aragon <keef.aragon@konscious.net>
2020-05-07 17:07:59 -0700

    Upgrade to 2.15.1 - note that this release won't work with SimpleStrategy just like 2.14 series wouldn't work with non-SSL

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ import os
 
 class DatastaxcppdriverConan(ConanFile):
     name = "datastax-cpp-driver"
-    version = "2.14.0"
+    version = "2.15.1"
     license = "Apache 2.0"
     url = "https://github.com/kmaragon/conan-datastax-cpp-driver"
     description = "Conan package for Datastax Open Source C++ driver (non-DSE)"
@@ -45,12 +45,10 @@ class DatastaxcppdriverConan(ConanFile):
         # This small hack might be useful to guarantee proper /MT /MD linkage
         # in MSVC if the packaged project doesn't have variables to set it
         # properly
-        tools.replace_in_file("cpp-driver-{}/CMakeLists.txt".format(self.version), "include(CppDriver)",
-                              '''include(CppDriver)
+        tools.replace_in_file("cpp-driver-{}/CMakeLists.txt".format(self.version), "project(cassandra C CXX)",
+                              '''project(cassandra C CXX)
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()''')
-
-        tools.replace_in_file("cpp-driver-{}/CMakeLists.txt".format(self.version), "CassUseLibuv()", '')
 
     def build(self):
         cmake = CMake(self)
@@ -65,12 +63,12 @@ conan_basic_setup()''')
         cmake.definitions["CASS_DEBUG_CUSTOM_ALLOC"] = "OFF"
         cmake.definitions["CASS_INSTALL_HEADER"] = "ON"
         cmake.definitions["CASS_INSTALL_PKG_CONFIG"] = "OFF"
+        cmake.definitions["LIBUV_ROOT_DIR"] = self.deps_cpp_info['libuv'].rootpath
         cmake.definitions["CASS_MULTICORE_COMPILATION"] = "ON" if self.options.multicore_compilation else "OFF"
         cmake.definitions["CASS_USE_BOOST_ATOMIC"] = "ON" if self.options.use_boost_atomic else "OFF"
         cmake.definitions["CASS_USE_STD_ATOMIC"] = "ON" if self.options.use_cpp_atomic else "OFF"
         cmake.definitions["CASS_USE_LIBSSH2"] = "OFF"
-        # FIXME: datastax is BROKEN without ssl on
-        cmake.definitions["CASS_USE_OPENSSL"] = "ON" # if self.options.use_openssl else "OFF"
+        cmake.definitions["CASS_USE_OPENSSL"] = "ON" if self.options.use_openssl else "OFF"
         cmake.definitions["CASS_USE_TCMALLOC"] = "ON" if self.options.use_tcmalloc else "OFF"
         cmake.definitions["CASS_USE_STATIC_LIBS"] = "ON"
         cmake.definitions["CASS_USE_ZLIB"] = "ON" if self.options.use_zlib else "OFF"
